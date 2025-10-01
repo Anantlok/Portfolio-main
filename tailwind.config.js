6a1faf4d90d4682d67a452a0151b0871dff76783
AnantLok <anant.alomano@gmail.com>
2025-10-02 00:20:24 +0530

    changed some stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     extend: {
       colors: {
         // --- New Natural Green Palette ---
-        'background': '#FDFBF4',      // Use for the main page background
+        'background': '#fffff0',      // Use for the main page background
         'surface': '#D0D8C3',         // Use for cards, modals, or secondary backgrounds
-        'primary': '#008F4C',         // Use for primary buttons, links, and important accents
+        'primary': '#034638',         // Use for primary buttons, links, and important accents
         'highlight-soft': '#D1DEA9',  // Use for hover states or subtle highlights
         'text-primary': '#073E1E',    // Use for headings and primary text for high contrast
       },
